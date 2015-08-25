f6ccf003609581de90b046a9cccabb5566c590d2
yskoh <koh.yesl@gmail.com>
2015-08-26 01:22:13 +0900

    stick falls, update scene doesn't work

--- ysStick.js
+++ ysStick.js
@@ -3,49 +3,44 @@ var context = canvas.getContext('2d');
 var stickLength = 0;
 var pressed = false;
 var columns = [];
+var colWidths =[10,20,25,40,45,60,65,,80,85,100,105,150,200];
+var colDistances =[50,100,150,200,250];
 var COL_HEIGHT = 200;
 var startX = 50;
 var ballRadius = 10;
 var stickWidth = 5;
-var ballMove =0;
+var ballMove = 0;
+var gameState = true;
+var nextLevel = false;
 
-var randWidth = 100;
-var randDistance = 100;
+var randWidth1 = random(colWidths);
+var randWidth2 = random(colWidths);
+var randDistance = random(colDistances);
 
-function updateColumns() {
-    if (columns.length === 2){
-        return;
-    }
-
-    while (columns.length < 2){
-        columns.push(makeColumn());
-    }
-
-    renderColumns();
+function random(array){
+	var max = array.length;
+	var min =0;
+	return array[Math.floor(Math.random() * (max - min) + min)];
 }
 
-function makeColumn(){
-	return {
-		width: randWidth,
-		height: COL_HEIGHT
-	}
-}
 //column
 function renderColumns(){
-	context.fillStyle = 'black';
-	context.fillRect(startX, canvas.height - COL_HEIGHT, columns[0].width, COL_HEIGHT);
+		var x = startX;
+		var y = canvas.height - COL_HEIGHT;
 
-	context.fillStyle ='blue';
-	context.fillRect(startX + columns[0].width + randDistance, canvas.height - COL_HEIGHT, columns[1].width, COL_HEIGHT);
+		context.fillStyle = 'black';
+		context.fillRect(x, y, randWidth1, COL_HEIGHT);
+
+		context.fillStyle ='blue';
+		context.fillRect(x + randWidth1 + randDistance, y, randWidth2, COL_HEIGHT);
 }
 //ball 	
 function renderBall(){
 	context.beginPath();
-	context.arc(startX + randWidth/2, canvas.height - COL_HEIGHT- ballRadius, ballRadius, 0, 2 * Math.PI, false);
+	context.arc(startX + randWidth1/2, canvas.height - COL_HEIGHT- ballRadius, ballRadius, 0, 2 * Math.PI, false);
 	context.fillStyle = 'pink';
 	context.fill();
 }
-
 
 function stickGrow(){
 	if(pressed){
@@ -56,69 +51,98 @@ function stickGrow(){
 	if(!pressed && stickLength === 0){
 		return;
 	}
-
-	// stickFall();
+	stickFall();
 }
 
 function renderStick(){
 	context.fillStyle ='gray';
-	context.fillRect(startX + columns[0].width - stickWidth, canvas.height - COL_HEIGHT - stickLength,stickWidth, stickLength);
+	context.fillRect(startX + randWidth1 - stickWidth, canvas.height - COL_HEIGHT - stickLength,stickWidth, stickLength);
 }
 
 //stick animation
+function radianToDegree(angle){
+	return angle/180*Math.PI;
+}
+
+var angle = -90;
+
 function stickFall(){
-	console.log("stick fall");
+	var cx = startX + randWidth1 - stickWidth;
+	var cy = canvas.height - COL_HEIGHT;
+	var endX = stickLength;
+	var endY = COL_HEIGHT;
+	// var length = stickLength;
+
+	// console.log("stick fall");
+	context.strokeStyle = "gray";
+	context.lineWidth = 4;
+
+	if(angle < 0){
+		angle+=3;
+	}
+	
+	endX = cx + Math.cos(radianToDegree(angle))*stickLength;
+	endY = cy + Math.sin(radianToDegree(angle))*stickLength; 
+
+  
+	context.moveTo(cx,cy);
+	context.lineTo(endX,endY);
+	context.stroke();
 }
 
 //ball animation
 function ballRoll(){
 	//if no bridge
-	// return;
-
-	//if long enough
-	context.clearRect(0,0, canvas.width, canvas.height);
-	renderColumns();
-	context.beginPath();
-	context.arc(startX + randWidth/2 + ballMove, canvas.height - COL_HEIGHT- ballRadius, ballRadius, 0, 2 * Math.PI, false);
-	context.fillStyle = 'pink';
-	context.fill();
-	if(ballMove < columns[0].width/2 + randDistance + columns[1].width/2){
-		ballMove +=5;
+	if(!stickLength){
+		return;
+	}
+	if(stickLength < randDistance || stickLength > randDistance + randWidth2){
+		console.log("DIE!!");
 	}
 
-	//if bridge not long
-	// die
+	if(stickLength > randDistance && stickLength < randDistance + randWidth2){
+		//if long enough
+		context.beginPath();
+		context.arc(startX + randWidth1/2 + ballMove, canvas.height - COL_HEIGHT- ballRadius, ballRadius, 0, 2 * Math.PI, false);
+		context.fillStyle = 'pink';
+		context.fill();
+		setTimeout(function(){
+			if(ballMove < randWidth1/2 + randDistance + randWidth2/2){
+				ballMove +=5;
+			}	
+			nextLevel = true;
+		}, 1500);
+		screenChange();
+	}
 }
 
 //screen change animation
 function screenChange(){
-	var prepNextBar = context.getImageData(50, 0, canvas.width - 50, canvas.height);
-	context.putImageData( prepNextBar,0, 0);
-	// context.clearRect(canvas.width-50, 0, 50, canvas.height);
+	var moveCol = 0;
 	if(nextLevel){
-		nextBar();
-		return;
+		if(randWidth1 + startX + randDistance > startX){
+			context.fillStyle='black';
+			context.fillRect(startX + randWidth1 + randDistance - moveCol,canvas.height - COL_HEIGHT, randWidth2, COL_HEIGHT);
+			moveCol= moveCol+10;
+		}
+		randWidth1 = randWidth2;
+		randWidth2 = random(colWidths);
+		context.fillRect(startX, canvas.height - COL_HEIGHT, randWidth2, COL_HEIGHT);
+		stickLength = 0;
 	}
 }
 
-function nextBar(){
-	console.log("reset");
-	stickLength =0;
-	columns.shift();
-
-	context.clearRect(0,0, canvas.width, canvas.height);
-}
 function draw(){	
-	updateColumns();
+	context.clearRect(0,0, canvas.width, canvas.height);
 	renderBall();
+	renderColumns();
 	stickGrow();
-	renderStick();
 	ballRoll();
-	screenChange();
 	window.requestAnimationFrame(draw);
 }
 
 window.requestAnimationFrame(draw);
+
 //mousedown event
 document.addEventListener('mousedown', function(){
 	pressed = true;
